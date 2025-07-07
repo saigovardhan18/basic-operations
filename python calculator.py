9e485838adbcba082b288b5db368951bc86ea0d3
saigovardhan18 <saigovardhan.stu@gmail.com>
2025-07-07 13:41:54 +0530

    Add files via upload

---
+++ python calculator.py
@@ -0,0 +1,21 @@
+print("SIMPLE CALCULATOR")
+
+num1 = input("Enter your first number: ")
+num2 = input("Enter your second number : ")
+
+print(f"You entered {num1} and {num2}")
+
+num1 = float(input("enter your first number: "))
+num2 = float(input("enter your second number: "))
+
+result = num1 + num2
+print(f"{num1} + {num2} = {result}")
+
+result = num1 - num2 
+print(f"{num1} - {num2} = {result}")
+
+result = num1*num2 
+print(f"{num1*num2} = {result}")
+
+result = num1/num2
+print(f"{num1/num2} = {result}")
